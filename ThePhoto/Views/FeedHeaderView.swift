55edd4c7359d1382ca4b916d0919e9eb8590c65d
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-04-03 04:24:29 +0300

    post liking done and feed header system changed

---
+++ ThePhoto/Views/FeedHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedHeaderView.swift
+//  ThePhoto
+//
+//  Created by Oğuzhan Abuhanoğlu on 3.04.2024.
+//
+
+import UIKit
+
+class FeedHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
